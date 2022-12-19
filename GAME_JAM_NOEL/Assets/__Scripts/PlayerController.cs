17240b101144e738fcf43d62d645f288471325a1
Asbel <91902256+ThomasChatrefou@users.noreply.github.com>
2022-12-19 18:14:33 +0100

    Add proto player input system + Change renderer to 2D

---
+++ GAME_JAM_NOEL/Assets/__Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    public float speedValue = 1.0f;
+    private Vector2 speedDirection;
+
+    public PlayerInputs inputs;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        inputs = new PlayerInputs();
+        inputs.Player.Shoot.performed += shootContext => Shoot();
+        inputs.Player.Movement.performed += moveContext => Move(moveContext.ReadValue<Vector2>());
+        inputs.Player.Movement.canceled += moveContext => Move(moveContext.ReadValue<Vector2>());
+    }
+
+    private void FixedUpdate()
+    {
+        transform.position += Time.fixedDeltaTime * speedValue * new Vector3(speedDirection.x, speedDirection.y, 0.0f);
+    }
+
+    private void Move(Vector2 direction)
+    {
+        speedDirection = direction;
+    }
+
+    private void Shoot()
+    {
+    }
+
+    private void OnEnable()
+    {
+        inputs.Enable();
+    }
+    private void OnDisable()
+    {
+        inputs.Disable();
+    }
+}
