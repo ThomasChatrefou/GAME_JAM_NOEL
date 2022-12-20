f0779b3261a6f794936c440d36aa551bbbb410e7
Azrod1 <91877581+Azrod1@users.noreply.github.com>
2022-12-20 14:57:05 +0100

    SpawnEnemyInWave

--- GAME_JAM_NOEL/Assets/__Scripts/GameLoop/EnemySpawn.cs
+++ GAME_JAM_NOEL/Assets/__Scripts/GameLoop/EnemySpawn.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
+using Random = UnityEngine.Random;
 
 [CreateAssetMenu(fileName = "Spawn", menuName = "ScriptableObjects/GameLoop/EnemySpawn")]
 public class EnemySpawn : ScriptableObject
@@ -18,4 +21,34 @@ public struct SpawnZone
 {
     [SerializeField] private Vector2 topLeft;
     [SerializeField] private Vector2 bottomRight;
+}
+
+
+// Class to spawn Enemy during the round
+public class SpawnEnemyInRound : EnemySpawn
+{
+    public int enemyInWave;
+    public int enemyInvoke;
+
+    private void NumberEnemy()
+    {
+        enemyInWave = Random.Range(MinEnemySpawn, MaxEnemySpawn);
+    }
+
+    // Spawn the requested number of enemies
+    private IEnumerator SpawnEnemy()
+    {
+       while(enemyInvoke<enemyInWave)
+        {
+            int indexEnemy = Random.Range(0, 3);
+            int indexSpawn = Random.Range(0, 5);
+
+            // Change "TypeOfEnemy" by a table where the different type of ennemies are stocked ; Change "PositionOfSpawner" by a table where the position of spawner are stocked
+            //Instantiate(TypeOfEnemy[indexEnemy], PositionOfSpawner[spawn], new Quaternion()); 
+
+            enemyInvoke++;
+
+            yield return new WaitForSeconds(0.2f); // Change value for spawning ennemies more and low fast
+        }
+    }
 }
