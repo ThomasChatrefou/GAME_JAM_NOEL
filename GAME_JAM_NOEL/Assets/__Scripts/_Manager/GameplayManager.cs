438c2a5e95798f4c61b78428f87b4f969eea223b
Azrod1 <91877581+Azrod1@users.noreply.github.com>
2022-12-20 13:43:18 +0100

    start sound design and try gameplay manager

--- GAME_JAM_NOEL/Assets/__Scripts/_Manager/GameplayManager.cs
+++ GAME_JAM_NOEL/Assets/__Scripts/_Manager/GameplayManager.cs
@@ -1,9 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class GameplayManager : MonoBehaviour
 {
+    public GameObject []typeEnemy;
+    public Vector3 []spawner;
+
+    #region
+    private float nbEnemyWave;
+    private int nbEnemyInvoke;
+    public int nbEnemyKill;
+    #endregion
+
     public GameplayManager instance;
     private void Awake()
     {
@@ -25,6 +35,52 @@ public class GameplayManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        WaveSystem();
+    }
+
+    // WaveSystem ------------------------------------------------------------------------------------------------------
+    private void WaveSystem()
+    {
+        if(nbEnemyInvoke != nbEnemyWave)
+        {
+            SpawnWave();
+        }
+        else if (nbEnemyKill == nbEnemyWave)
+        {
+            EndWave();
+        }
+    }
+
+    private void StartWave()
+    {
+        nbEnemyWave = Mathf.Round(nbEnemyWave * 1.5f);
+        nbEnemyInvoke = 0;
+        nbEnemyKill = 0;
+    }
+
+    private IEnumerator SpawnWave()
+    {
+        while(nbEnemyInvoke < nbEnemyWave)
+        {
+            int number = Random.Range(0,3);
+            int spawn = Random.Range(0,5);
+
+            Instantiate(typeEnemy[number], spawner[spawn], new Quaternion());
+
+            yield return new WaitForSeconds(0.2f);
+        }
+    }
+
+    private IEnumerator EndWave()
+    {
+        RespawnPlayer();
+        yield return new WaitForSeconds(2.0f);
+        StartWave();
+    }
+    // EndWaveSystem ---------------------------------------------------------------------------------------------------
+
+    private void RespawnPlayer()
+    {
+
     }
 }
